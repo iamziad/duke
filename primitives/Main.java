559d91dac3b4f090af8779e0c55cd4cc49942cb4
Ziad Ahmed <dev.ziadahmed@gmail.com>
2026-03-30 01:16:08 +0200

    first program

---
+++ primitives/Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int maxInt = Integer.MAX_VALUE;
+        float f = 0.5f;
+        int x = 5;
+        boolean isFun = true;
+
+        int overflowInt1 = Integer.MAX_VALUE + 1; // will compile
+        int overflowInt2 = 2147483647 + 1; // will compile
+        int overflowInt3 = 2147483648; // will crash
+        int overflowInt4 = 2_147_483_647; // underscore separator
+
+        System.out.println(""+ ++f +"");
+        System.out.println("Maximum integer = " + maxInt + "");
+        System.out.println(isFun ? "yes" : "no");
+    }
+}
